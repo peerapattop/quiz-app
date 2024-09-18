073a9562abb7bc8a52dfe6e04617588509573140
peerapattop <peerapatza401@gmail.com>
2024-09-18 14:49:53 +0700

    Update start_screen.dart

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,6 +12,7 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(height: 80),
           const Text(
@@ -22,7 +23,7 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
-          TextButton.icon(
+          OutlinedButton.icon(
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
