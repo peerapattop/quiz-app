af3ca236e485cc090ae9aab2e220c22746731441
peerapattop <peerapatza401@gmail.com>
2024-09-18 14:21:45 +0700

    Initial commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          TextButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            child: const Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
+  }
+}
