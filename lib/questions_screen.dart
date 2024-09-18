37163e10532ddae5d755826e62f2857d807e4db3
peerapattop <peerapatza401@gmail.com>
2024-09-18 23:05:51 +0700

    create widget answer button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basic/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -10,6 +11,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Quiz Screen");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: "answer 1", onTap: (){}),
+          AnswerButton(answerText: "answer 2", onTap: (){}),
+          AnswerButton(answerText: "answer 3", onTap: (){}),
+        ],
+      ),
+    );
   }
 }
